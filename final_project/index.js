c80d3927f09df267f3e4b3591f3eca699d2005e5
07kish <07kishorevenkatraman@gmail.com>
2023-02-05 04:13:14 -0500

    commit changes

--- final_project/index.js
+++ final_project/index.js
@@ -7,16 +7,24 @@ const genl_routes = require('./router/general.js').general;
 const app = express();
 
 app.use(express.json());
+app.use(session({ secret: "fingerprint", resave: true, saveUninitialized: true }))
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    //Write the authenication mechanism here
+    if (req.session?.authorization?.username) {
+        next()
+    }
+    else {
+        console.error("Login Failed")
+        return res.status(401).json({ code: 401, message: 'Login failed' })
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
